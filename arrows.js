e80d196fe9e0b3b0ec05ac98bd10c4da153bd331
parvez kose <flamboyanz@gmail.com>
2016-01-15 10:32:42 -0500

    formatted arrows

--- arrows.js
+++ arrows.js
@@ -2,6 +2,7 @@
 
 let square = x => x * x;
 let add = (a, b) => a + b;
+let fun = (a, b) => { return a + b; };
 let pi = () => 3.1415;
 
 console.log(square(5));
@@ -11,50 +12,31 @@ console.log(pi());
 /* example 2 */
 
 ;(function(){
-
     'use strict';
-
-    var f = (a, b) => {
-        let c = [a, b];
-        return c;
-    };
-
+    var f = (a, b) => { let c = [a, b];
+        return c; };
     console.log(f(1, 2));
-
 }.call({'a':1}));
 
-/* example 3 */
-
-
 ;(function(){
-
     'use strict';
-
     var f = () => this;
-
     console.log(f());
-
 }.call({'a':1}));
 
 ;(function(){
-
     'use strict';
-
     var f = (a) => a;
-
-    console.log(f(1));
-
+    console.log(f(1))
 }.call({'a':1}));
 
 ;(function(){
-
     'use strict';
-
     var f = (a, b) => [a, b];
-
     console.log(f(1, 2));
-
 }.call({'a':1}));
+
+/* example 3 - lexical this - Invoked through call or apply */
 
 var adder = {
     base : 5,
@@ -79,3 +61,27 @@ var adder = {
 
 console.log(adder.add(1));         // This would log to 2
 console.log(adder.addThruCall(1)); // This would log to 2 still
+
+/* example 4 - lexical arguments  */
+//Arrow functions do not expose an arguments object
+
+var arguments = 42;
+var arr = () => arguments;
+
+arr(); // 42
+
+function foo() {
+    var f = (i) => arguments[0]+i; // foo's implicit arguments binding
+    return f(2);
+}
+
+foo(1); // 3
+
+// rest parameters are good alternatives to arguments
+
+function foo() {
+    var f = (...args) => args[0];
+    return f(2);
+}
+
+foo(1); // 2
