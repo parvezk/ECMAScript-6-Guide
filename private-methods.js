915b31b3482467313869485a030b5ad3135c957c
Parvez Kose <flamboyanz@gmail.com>
2018-01-19 22:29:36 -0500

    day 19 coding

---
+++ private-methods.js
@@ -0,0 +1,37 @@
+// Private ES6 methods
+
+// apple.js
+
+const transformName = Symbol('private:transformName');
+
+class Apple {
+  constructor () {
+    console.log('i am created')
+  }
+
+  [transformName] (name) {
+    return name.split('').reverse().join('');
+  }
+
+  fallOn (person) {
+    return `Fell on ${this[transformName](person)}`;
+  }
+}
+
+export default Apple;
+
+// main.js
+import React from 'react';
+import Apple from './apple';
+
+function Hello () {
+  
+  const apple = new Apple();
+  const txt = apple.fallOn('notweN');
+
+  return (
+    <h1>Hello.. {txt}</h1> 
+  )
+}
+
+export default Main;
