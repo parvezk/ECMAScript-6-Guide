d0101394226973aaabd7c3af8eaf5d409065122b
Parvez Kose <flamboyanz@gmail.com>
2018-01-23 22:15:09 -0500

    day 22 coding update

--- let-const-block-scoping.js
+++ let-const-block-scoping.js
@@ -18,3 +18,20 @@ function updateProductId() {
 let productId = null;
 updateProductId();
 console.log(productId);
+
+let productId = 42;
+for (let productId = 0; productId < 10; productId++){
+
+}
+
+let updateFunctions = [];
+for(let i = 0; i <= 2; i++){
+    updateFunctions.push(function(){ return i; });
+}
+//console.log(updateFunctions[2]());
+
+const MARCKUP_PCT = 100;
+if (MARCKUP_PCT > 0){
+    const MARCKUP_PCT = 10;
+}
+//console.log(MARCKUP_PCT);
