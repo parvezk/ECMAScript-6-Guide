19e9b8b1619dccd3ed4b09edcd7975641c264bee
Parvez Kose <flamboyanz@gmail.com>
2018-02-04 21:19:46 -0500

    day 35 coding

---
+++ def-fn-param.js
@@ -0,0 +1,31 @@
+// Default Function Parameter *******************************
+
+var getProduct = (productId = 1000) => console.log(productId);
+//getProduct();
+
+var getProduct2 = (productId = 1000, type = 'software') => {
+    console.log(productId, type)
+}
+//getProduct2(undefined, 'hardware');
+
+var getTotal = function(price, tax = price * 0.07){
+    console.log(price, tax)
+}
+//getTotal(5.00);
+
+const baseTax = 0.07;
+var getTotal2 = (price, tax = price * baseTax) => {
+    console.log(price, tax);
+};
+//getTotal2(5.00);
+
+var generateBaseTax = () => 0.07;
+var getTotal3 = (price, tax = price * generateBaseTax()) => {
+    console.log(price, tax);
+}
+//getTotal3(5.00);
+
+var getTotal4 = (price, tax = 0.07) => {
+    console.log(arguments.length, typeof arguments[0]);
+}
+//getTotal4(5.00);
---
+++ let-const-block-scope.js
@@ -0,0 +1,38 @@
+
+// let, const and Block scoping *******************************
+'use strict';
+
+let productId = 12;
+{
+    let productId = 2000;
+}
+console.log(productId);
+
+{
+    let productId2 = 2000;
+}
+console.log(productId2);
+
+function updateProductId() {
+    productId = 12;
+}
+let productId = null;
+updateProductId();
+console.log(productId);
+
+let productId = 42;
+for (let productId = 0; productId < 10; productId++){
+
+}
+
+let updateFunctions = [];
+for(let i = 0; i <= 2; i++){
+    updateFunctions.push(function(){ return i; });
+}
+//console.log(updateFunctions[2]());
+
+const MARCKUP_PCT = 100;
+if (MARCKUP_PCT > 0){
+    const MARCKUP_PCT = 10;
+}
+//console.log(MARCKUP_PCT);
