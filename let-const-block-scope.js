b5aa984513bfe4ba69d4cc5b32f6478f8738c608
Parvez Kose <flamboyanz@gmail.com>
2019-07-03 17:48:32 -0400

    update

--- let-const-block-scope.js
+++ let-const-block-scope.js
@@ -1,49 +1,54 @@
-
 // let, const and block scoping *******************************
-'use strict';
+"use strict";
 
 let set = Array.from(Array(N), (x, index) => {
-    return 0;
+  return 0;
 });
 
 for (const [index, value] of set.entries()) {
-    if (value) {
-      if (isNextMoveValid(value, index))
-        movables.push(value);
-    }
+  if (value) {
+    if (isNextMoveValid(value, index)) movables.push(value);
   }
+}
+
+for (const [index, value] of posUpdates.entries()) {
+  const x = Object.keys(value)[0];
+  if (x == movableItem) {
+    value[movableItem] += index;
+  }
+}
 
 let productId = 12;
 {
-    let productId = 2000;
+  let productId = 2000;
 }
 console.log(productId);
 
 {
-    let productId2 = 2000;
+  let productId2 = 2000;
 }
 console.log(productId2);
 
 function updateProductId() {
-    productId = 12;
+  productId = 12;
 }
 let productId = null;
 updateProductId();
 console.log(productId);
 
 let productId = 42;
-for (let productId = 0; productId < 10; productId++){
-
-}
+for (let productId = 0; productId < 10; productId++) {}
 
 let updateFunctions = [];
-for(let i = 0; i <= 2; i++){
-    updateFunctions.push(function(){ return i; });
+for (let i = 0; i <= 2; i++) {
+  updateFunctions.push(function() {
+    return i;
+  });
 }
 //console.log(updateFunctions[2]());
 
 const MARCKUP_PCT = 100;
-if (MARCKUP_PCT > 0){
-    const MARCKUP_PCT = 10;
+if (MARCKUP_PCT > 0) {
+  const MARCKUP_PCT = 10;
 }
 //console.log(MARCKUP_PCT);
